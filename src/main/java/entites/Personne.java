f5e56232b6eb9b99a01b19783e87200f6046f3e7
Salaheddine EL Majdoub <salahmajdoub41@gmail.com>
2020-03-03 18:53:44 +0100

    TP3: Personne

--- src/main/java/entites/Personne.java
+++ src/main/java/entites/Personne.java
@@ -1,11 +1,41 @@
 package entites;
 
 public class Personne {
+	
 
 	 public String nom;
 	 public String prenom;
 	 public AdressePostale adresse;
 
+	public Personne(String nom, String prenom, AdressePostale adresse) {
+		this.nom = nom;
+		this.prenom = prenom;
+		this.adresse = adresse;
 	}
+    public void AfficherIdentite() {
+    	System.out.println (prenom + " " + nom.toUpperCase());
+    }
+	public String getNom() {
+		return nom;
+	}
+	public void setNom(String nom) {
+		this.nom = nom;
+	}
+	public String getPrenom() {
+		return prenom;
+	}
+	public void setPrenom(String prenom) {
+		this.prenom = prenom;
+	}
+	public AdressePostale getAdresse() {
+		return adresse;
+	}
+	public void setAdresse(AdressePostale adresse) {
+		this.adresse = adresse;
+	}
+    
+      
+	}
+
 
 
--- src/main/java/essais/TestPersonne.java
+++ src/main/java/essais/TestPersonne.java
@@ -6,17 +6,25 @@ import entites.Personne;
 public class TestPersonne {
 
 	public static void main(String[] args) {
-		 AdressePostale a = new AdressePostale ();
-		 a.numeroRue = 5;
-		 a.libelleRue = "chemin du bassin";
-		 a.codePostale= 92000;
-         a.ville = "Paris";
-         
-         Personne p = new Personne();
-         p.nom = "EL MAJDOUB";
-         p.prenom = "Salaheddine";
-         p.adresse = a;
+		AdressePostale a1 = new AdressePostale (5, "chemin du bassin", 92000, "Paris");
+		Personne p1 = new Personne ("EL MAJDOUB", "Salaheddine", a1);
+		
+		AdressePostale a2 = new AdressePostale (5, "chemin du bassin", 92000, "Paris");
+	    Personne p2 = new Personne (" majdoub", "Salah", a2);
+    
+	   p1.AfficherIdentite();
+       p2.AfficherIdentite();
+       
+       p1.setNom("el majdoub");
+       p1.setPrenom("salaheddine");
+       p1.setAdresse(a1);
+       
+       String n1=p1.getNom();
+       System.out.println(n1);
+       
 
 	}
+	
+	
+	}
 
-}
